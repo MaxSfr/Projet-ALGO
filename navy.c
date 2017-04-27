32cba1f02cbb7b12fa27ab5fbb08e8a517d067a7
S.Max <maxime.serree@edu.univ-fcomte.fr>
2017-04-27 17:10:08 +0200

    Update navy.c
    Problème lors de l'envoi au serveur, Ne marche pas toujours

--- navy.c
+++ navy.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
 
 //Taille des bateaux
 #define PA 5
@@ -8,109 +11,135 @@
 #define SM 3
 #define Tr 2
 
+//Définition du BUZSIZE
+#define BUFSIZE 256
+
 //Définition des structures
 struct position{
-  int positionY;
-  char positionX;
+  	int positionY;
+  	char positionX;
 };
 
 struct Mine{
-  size_t size;
-  size_t capacity;
-  struct position *data;
+  	size_t size;
+  	size_t capacity;
+  	struct position *data;
 };
 
 void positionMine(struct Mine *self){
-  self->data = malloc (sizeof(struct position)*5);
+  	self->data = malloc (sizeof(struct position)*5);
 
-  self->data[0].positionY = 1;
-  self->data[0].positionX = 'A';
+  	self->data[0].positionY = 1;
+  	self->data[0].positionX = 'A';
 
-  self->data[1].positionY = 1;
-  self->data[1].positionX = 'A';
+  	self->data[1].positionY = 1;
+  	self->data[1].positionX = 'A';
 
-  self->data[2].positionY = 1;
-  self->data[2].positionX = 'A';
+  	self->data[2].positionY = 1;
+  	self->data[2].positionX = 'A';
 
-  self->data[3].positionY = 1;
-  self->data[3].positionX = 'A';
+  	self->data[3].positionY = 1;
+  	self->data[3].positionX = 'A';
 
-  self->data[4].positionY = 1;
-  self->data[4].positionX = 'A';
+  	self->data[4].positionY = 1;
+  	self->data[4].positionX = 'A';
 
-  self->size = 5;     //A chauqe fois qu'une mine est détruite alors on enlève 1 à la taille
-  self->capacity = 5; //On ne peut avoir seulement 5 mines, la capacité ne changera jamais
+  	self->size = 5;     //A chauqe fois qu'une mine est détruite alors on enlève 1 à la taille
+  	self->capacity = 5; //On ne peut avoir seulement 5 mines, la capacité ne changera jamais
 
 
 }
 
 struct Bateau{
-  int tailleBateau;
-  struct position positionTop;
-  struct position positionBottom;
+  	int tailleBateau;
+  	struct position positionTop;
+  	struct position positionBottom;
 };
 
 struct Equipe {
-  size_t size;
-  size_t capacity;
-  struct Bateau *data;
+  	size_t size;
+  	size_t capacity;
+  	struct Bateau *data;
 };
 
 void positionInitialiser(struct Equipe *self){
-  self->data = malloc (sizeof(struct Bateau)*5);
+  	self->data = malloc (sizeof(struct Bateau)*5);
   
   //on défini la postion du porte avions ainsi que ca taille
-  self->data[0].tailleBateau = PA;
-  self->data[0].positionTop.positionY = 2;
-  self->data[0].positionTop.positionX = 'B';
-  self->data[0].positionBottom.positionY = 2;
-  self->data[0].positionBottom.positionX = 'B';
+  	self->data[0].tailleBateau = PA;
+  	self->data[0].positionTop.positionY = 2;
+  	self->data[0].positionTop.positionX = 'B';
+  	self->data[0].positionBottom.positionY = 2;
+  	self->data[0].positionBottom.positionX = 'F';
 
   //on défini la postion du croiseur ainsi que ca taille
-  self->data[1].tailleBateau = Cr;
-  self->data[1].positionTop.positionY = 1;
-  self->data[1].positionTop.positionX = 'C';
-  self->data[1].positionBottom.positionY = 1;
-  self->data[1].positionBottom.positionX = 'F';
+  	self->data[1].tailleBateau = Cr;
+  	self->data[1].positionTop.positionY = 1;
+  	self->data[1].positionTop.positionX = 'C';
+  	self->data[1].positionBottom.positionY = 1;
+  	self->data[1].positionBottom.positionX = 'F';
 
   //on défini la postion du contre_torpilleur ainsi que ca taille
-  self->data[2].tailleBateau = CT;
-  self->data[2].positionTop.positionY = 7;
-  self->data[2].positionTop.positionX = 'C';
-  self->data[2].positionBottom.positionY = 7;
-  self->data[2].positionBottom.positionX = 'E';
+  	self->data[2].tailleBateau = CT;
+  	self->data[2].positionTop.positionY = 7;
+  	self->data[2].positionTop.positionX = 'C';
+  	self->data[2].positionBottom.positionY = 7;
+  	self->data[2].positionBottom.positionX = 'E';
 
   //on défini la postion du sous_marin ainsi que ca taille
-  self->data[3].tailleBateau = SM;
-  self->data[3].positionTop.positionY = 4;
-  self->data[3].positionTop.positionX = 'D';
-  self->data[3].positionBottom.positionY = 4;
-  self->data[3].positionBottom.positionX = 'F';
+  	self->data[3].tailleBateau = SM;
+  	self->data[3].positionTop.positionY = 4;
+  	self->data[3].positionTop.positionX = 'D';
+  	self->data[3].positionBottom.positionY = 4;
+  	self->data[3].positionBottom.positionX = 'F';
 
   //on défini la postion du torpilleur ainsi que ca taille
-  self->data[4].tailleBateau = Tr;
-  self->data[4].positionTop.positionY = 5;
-  self->data[4].positionTop.positionX = 'F';
-  self->data[4].positionBottom.positionY = 7;
-  self->data[4].positionBottom.positionX = 'A';
+  	self->data[4].tailleBateau = Tr;
+  	self->data[4].positionTop.positionY = 5;
+  	self->data[4].positionTop.positionX = 'F';
+  	self->data[4].positionBottom.positionY = 6;
+  	self->data[4].positionBottom.positionX = 'F';
 
-  self->size = 5;
-  self->capacity = 5;
+  	self->size = 5;
+ 	self->capacity = 5;
 };
 
 
 int main(){
-  struct Equipe NotreEquipe;
-  struct Mine NosMine;
+  	struct Equipe NotreEquipe;
+  	struct Mine NosMine;
 
-  //Initialisation de la position des Mines et des Bateaux
-  positionMine(&NosMine);
-  positionInitialiser(&NotreEquipe);
+  	setbuf(stdout, NULL);
+  	char buffer[BUFSIZE];
 
-  //affichage des coordonnées des bateaux;
-  for(size_t i = 0; i < NotreEquipe.size ; i++){
-    printf("PositionDébutBateau %d %c PositionFinBateau %d %c\n",NotreEquipe.data[i].positionTop.positionY,NotreEquipe.data[i].positionTop.positionX,NotreEquipe.data[i].positionBottom.positionY,NotreEquipe.data[i].positionBottom.positionX );
-  }
+  	//Initialisation de la position des Mines et des Bateaux
+  	positionMine(&NosMine);
+  	positionInitialiser(&NotreEquipe);
 
-  return 0;
+  	printf("C2\n");//Ne pas oublier de faire la lecture de nos mines et pas de celle ci
+  	printf("H2\n");
+  	printf("C7\n");
+  	printf("H7\n");
+  	printf("E5\n");
+
+	
+
+  	//affichage des coordonnées des bateaux
+  	for(size_t i = 0; i < NotreEquipe.size ; i+=1){
+    		printf("%c%d%c%d\n",NotreEquipe.data[i].positionTop.positionX,NotreEquipe.data[i].positionTop.positionY,NotreEquipe.data[i].positionBottom.positionX,NotreEquipe.data[i].positionBottom.positionY );
+    
+    		fgets(buffer, BUFSIZE, stdin);
+		assert(strcmp(buffer, "OK\n") == 0);
+  	}
+
+	/*for (;;) {
+    		printf("SHOOT\n"); // or POOL or MOVE
+    		printf("D2\n");
+    		fgets(buffer, BUFSIZE, stdin);
+    		fgets(buffer, BUFSIZE, stdin); // NOTHING or ATTACK
+  	}*/
+	
+	
+
+  	return EXIT_SUCCESS;
 }
